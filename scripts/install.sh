1a31a8d67da5dcaa4daabb5c0a103b322b0f480a
gtsop <gtsop+github@protonmail.com>
2026-04-21 20:25:12 +0300

    feat: deploy the nginx website

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,7 @@ cd $SCRIPT_DIR/../
 
 RPI_NODE_BIN=$(realpath ./workspaces/rpi-node/bin/)/rpi-node
 RPI_NODE_API_BIN=$(realpath ./workspaces/rpi-node/bin/)/rpi-node-api
+RPI_NODE_FE_ROOT=$(realpath ./workspaces/rpi-node/frontend/public)
 
 sed "s%RPI_NODE_BIN%$RPI_NODE_BIN%g" ./workspaces/rpi-node/rpi-node.service.template > rpi-node.service
 sudo mv rpi-node.service /lib/systemd/system/rpi-node.service
@@ -20,5 +21,16 @@ sudo mv rpi-node-api.service /lib/systemd/system/rpi-node-api.service
 echo "To run the pegion defence api service run:"
 echo ""
 echo "    systemctl start rpi-node-api"
+echo ""
+
+sudo mkdir -p /var/www/rpi-node-fe
+sudo cp -R $RPI_NODE_FE_ROOT/* /var/www/rpi-node-fe
+
+sudo mkdir -p /etc/nginx/nginx.d/
+sudo cp ./workspaces/rpi-node/frontend/nginx/nginx.conf /etc/nginx/nginx.conf
+
+echo "To run the pegion defence frontend service run:"
+echo ""
+echo "    systemctl start nginx"
 echo ""
 
