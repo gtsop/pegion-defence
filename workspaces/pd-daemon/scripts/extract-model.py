d26a2a008d34da7dc89ac056e7b661e6138b749b
gtsop <gtsop+github@protonmail.com>
2026-04-22 19:28:35 +0300

    chore: add a script for exporting the model

--- workspaces/pd-daemon/scripts/extract-model.py
+++ workspaces/pd-daemon/scripts/extract-model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-uv run yolo export project=./models model=yolov8n.pt format=ncnn half=True imgsz=320 path=./models
+cd ./models
+uv run yolo export project=./models model=yolov8n.pt format=ncnn half=True imgsz=320
