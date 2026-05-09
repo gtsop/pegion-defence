976b6270502515c1299b322aa50d19f9df4eb5a2
gtsop <gtsop+github@protonmail.com>
2026-05-09 17:29:31 +0300

    --amend

--- workspaces/pd-node/pd_node/db/__init__.py
+++ workspaces/pd-node/pd_node/db/__init__.py
@@ -5,7 +5,7 @@ from . import models
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url, echo=True)
+engine = create_engine(sqlite_url, echo=False)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
